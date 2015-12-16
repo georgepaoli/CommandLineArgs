f1581488d37335efdc31403c7fa2f5bd7d57a35d
Krzysztof Wicher <kwicher@microsoft.com>
2015-12-15 22:25:45 -0800

    Add Zip command

---
+++ src/Zip/Program.cs
@@ -0,0 +1,12 @@
+﻿using CommandLineArgs;
+
+namespace Zip
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            return ConsoleApp.StartApp<Program>(args);
+        }
+    }
+}
---
+++ src/Zip/Zip.cs
@@ -0,0 +1,30 @@
+﻿using CommandLineArgs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO.Compression;
+
+namespace Zip
+{
+    public class Zip
+    {
+        [Required]
+        [PopArg]
+        public string ZipPath;
+
+        [Required]
+        [PopArg]
+        public string Directory;
+
+        public void Pack()
+        {
+            ZipFile.CreateFromDirectory(Directory, ZipPath);
+        }
+
+        public void Extract()
+        {
+            ZipFile.ExtractToDirectory(ZipPath, Directory);
+        }
+    }
+}
